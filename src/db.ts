1eadf28309fb8096d2221009d4bdde25356c9fe8
Your Name <you@example.com>
2023-07-11 15:22:35 -0500

    base de datos deployada

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 (async ()=>{
     try {
-        await mongoose.connect("mongodb://localhost/ulti",)
+        await mongoose.connect("mongodb+srv://juanca:juanca@cluster0.muchvce.mongodb.net/util",)
         console.log("db")    
     } catch (error) {
         console.log(error)   
